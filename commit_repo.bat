3d81db02560981482728abaea144abb84850b304
Lukas Skarpa <lukskarpa@gmail.com>
2026-07-19 01:41:03 +0200

    Clean up: remove helper scripts, finalize repo

--- commit_repo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "c:\Users\luky\Desktop\cline documents"
-"C:\Program Files\Git\bin\git.exe" add profile_photo.png
-"C:\Program Files\Git\bin\git.exe" add rendercv_output/profile_photo.png
-"C:\Program Files\Git\bin\git.exe" add clean_repo.bat
-"C:\Program Files\Git\bin\git.exe" add commit_repo.bat
-"C:\Program Files\Git\bin\git.exe" commit -m "Initial commit: CV with photo, cleaned up repo"
-"C:\Program Files\Git\bin\git.exe" status --short
-echo Done.
